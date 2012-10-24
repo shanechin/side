3ed850448d96c157ec04facf103b578a77b81b1e
Shane Chin <sac2171@gmail.com>
2012-10-23 22:08:25 -0400

    routing urls

--- cribs/cribs/client/cribs.js
+++ cribs/cribs/client/cribs.js
@@ -1,6 +1,22 @@
 "use strict";
 
+var DormRouter = Backbone.Router.extend({
+	routes:{
+		//routeMatcher:routeName
+		"*home": "defaultRoute",
+		":dorm_id": "main"
+	},
+	main:function(dorm_id){
+		console.log("We routed" + dorm_id);
+		this.navigate(dorm_id,true);
+	},
+	defaultRoute:function(home){
+		console.log("We routed home?");
+	}
+});
+
 var Dorms = new Meteor.Collection('dorms');
+var Router = new DormRouter;
 
 if (Meteor.isClient) {
 
@@ -13,7 +29,15 @@ if (Meteor.isClient) {
 	Template.dorm.events({
 		'click':function(){
 			console.log("dorm clicked!" + this._id + this.name);
+			Router.main(this._id);
+			Session.set('this_dorm', this._id);
 		}
-	})
-
+	});
+	
 }
+
+
+Meteor.startup(function(){
+	//Alows for bookmarkable urls
+	Backbone.history.start({pushState:true});
+});
